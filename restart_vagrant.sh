7571b03df2a197583ceeb0e720a5d289d4ebf73c
Mikael Lepistö <mikael.lepisto@vincit.fi>
2019-10-31 12:18:56 +0200

    Fixed packer provisioning scricpt to work with tcsh and tried to make host connection from vagrant VM instance to work

--- restart_vagrant.sh
+++ restart_vagrant.sh
@@ -2,4 +2,5 @@ set -x
 vagrant destroy -f; 
 vagrant box remove file://packer_virtualbox-iso_virtualbox.box; 
 vagrant box remove packer_virtualbox-iso_virtualbox.box; 
-VAGRANT_LOG=debug vagrant up
+vagrant up;
+#VAGRANT_LOG=debug vagrant up
