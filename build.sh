e433245daaad367a88ae3e8984d3aa445a964c16
Mikael Lepistö <mikael.lepisto@vincit.fi>
2019-11-22 13:55:44 +0200

    Removed old link to a packer config which I used as a study matrial (among many others) for writing this

--- build.sh
+++ build.sh
@@ -5,6 +5,3 @@ VBoxManage unregistervm --delete packer-pfSense
 sleep 3
 rm -fr output-pfSense
 PACKER_LOG=1 /Users/mikaelle/mytools/packer build -on-error=abort pfSense-conf.json
-
-# To create separate vagrant user check out 
-# https://github.com/chrisanthropic/packer-pfsense/blob/master/pfsense.json
