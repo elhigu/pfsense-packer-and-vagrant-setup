fc633bdb350e1e2a0e31d0355ceb74ea39470716
Mikael Lepistö <mikael.lepisto@vincit.fi>
2019-11-24 19:21:59 +0200

    Updated pfSense install image to version 2.4.4 p3

--- download-installer.sh
+++ download-installer.sh
@@ -1,2 +1,2 @@
-wget https://nyifiles.pfsense.org/mirror/downloads/pfSense-CE-2.4.4-RELEASE-p1-amd64.iso.gz 
-gunzip pfSense-CE-2.4.4-RELEASE-p1-amd64.iso.gz
+wget https://nyifiles.pfsense.org/mirror/downloads/pfSense-CE-2.4.4-RELEASE-p3-amd64.iso.gz 
+gunzip pfSense-CE-2.4.4-RELEASE-p3-amd64.iso.gz
